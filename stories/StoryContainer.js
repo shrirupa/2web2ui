a776c06f217cddbb1999ed301aa2e02ee879981a
Ewan Dennis <ewandennis@users.noreply.github.com>
2019-02-13 12:34:21 +0000

    Stories now support Modals

--- stories/StoryContainer.js
+++ stories/StoryContainer.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import {
-  BrowserRouter as Router
-} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
+const StoryContainer = ({ bg, children }) => {
+  // Modals need this portal element in place _before_ they render.
+  const modalPortal = document.createElement('div');
+  modalPortal.id = 'modal-portal';
+  document.body.appendChild(modalPortal);
 
-const StoryContainer = ({ bg, children }) => (
-  <Router>
-    <div style={{ padding: '30px', minHeight: '100vh', background: bg || '#f2f2f5' }}>
-      {children}
+  return <Router>
+    <div>
+      <div
+        style={{
+          padding: '30px',
+          minHeight: '100vh',
+          background: bg || '#f2f2f5'
+        }}
+      >
+        {children}
+      </div>
     </div>
-  </Router>
-);
+  </Router>;
+};
 
 export default StoryContainer;
