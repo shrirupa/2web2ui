d03b30e79b2bfd4d970f0436a774be02840394bc
Ewan Dennis <ewandennis@users.noreply.github.com>
2019-02-22 12:13:27 +0000

    Move portal modal setup to iframe

--- stories/StoryContainer.js
+++ stories/StoryContainer.js
@@ -2,11 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 const StoryContainer = ({ bg, children }) => {
-  // Modals need this portal element in place _before_ they render.
-  const modalPortal = document.createElement('div');
-  modalPortal.id = 'modal-portal';
-  document.body.appendChild(modalPortal);
-
   return <Router>
     <div>
       <div
