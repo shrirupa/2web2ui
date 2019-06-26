c1fb29515319a25c67ab328aa3c178f62cc0cf39
mhossain <mohammad.hossain@sparkpost.com>
2019-06-26 12:51:16 -0400

    sync with local storage only at unmount of test data editor

--- src/pages/templatesV2/components/TestDataSection.js
+++ src/pages/templatesV2/components/TestDataSection.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import useEditorContext from '../hooks/useEditorContext';
 import Editor from './Editor';
 
 const TestDataSection = () => {
-  const { setTestData, testData } = useEditorContext();
+  const { setTestData, syncTestDataInStorage, testData } = useEditorContext();
+
+  useEffect(() => //sync with storage during unmount
+    syncTestDataInStorage
+  , [syncTestDataInStorage]);
 
   return (
     <Editor
       mode="json"
       name="test-data-editor"
       onChange={(val) => {
-        setTestData(val);
+        try {
+          setTestData(JSON.parse(val));
+        } catch (err) {} /* eslint no-empty: ["error", { "allowEmptyCatch": true }] */
       }}
       value={testData}
     />
--- src/pages/templatesV2/hooks/useTestData.js
+++ src/pages/templatesV2/hooks/useTestData.js
@@ -1,32 +1,40 @@
 import { useEffect, useMemo, useState } from 'react';
 import isEqual from 'lodash/isEqual';
 
-const useTestData = ({ draft = {}, templateTestData, version: mode, getTestDataFromLocalStorage, setTestDataToLocalStorage, isPublishedMode }) => {
+const useTestData = ({ draft = {}, templateTestData, getTestDataFromLocalStorage, setTestDataToLocalStorage, isPublishedMode }) => {
   const [state, setState] = useState(templateTestData);
+  const templateId = draft.id;
+  const mode = isPublishedMode ? 'published' : 'draft';
 
-  //on mount, trigger fetching data from store
+  //trigger fetching data from store.
   useEffect(() => {
-    getTestDataFromLocalStorage({ id: draft.id, mode });
-  }, [draft.id, getTestDataFromLocalStorage, mode]);
+    getTestDataFromLocalStorage({ id: templateId, mode });
+  }, [getTestDataFromLocalStorage, mode, templateId]);
 
   //update local state when testData is externally updated (e.g. completed loading from store)
   useEffect(() => {
-    if (!isEqual(state, templateTestData)) {
-      setState(templateTestData);
+    setState(templateTestData);
+  }, [templateTestData, mode]);
+
+  const formattedData = useMemo(() => {
+    try {
+      return JSON.parse(state);
+    } catch (err) {
+      return {};
     }
-  }, [setState, state, templateTestData]);
+  }, [state]);
 
-  //whenever data is changed locally, save it in localstorage.
-  useEffect(() => {
-    setTestDataToLocalStorage({ id: draft.id, data: state, mode });
-  }, [state, draft.id, mode, setTestDataToLocalStorage]);
-
-  const formattedData = useMemo(() => JSON.parse(state), [state]);
+  const syncTestDataInStorage = () => {
+    if (!isEqual(templateTestData, state)) {
+      setTestDataToLocalStorage({ id: templateId, data: state, mode });
+    }
+  };
 
   return {
     formattedTestData: formattedData,
     testData: state,
-    setTestData: setState
+    setTestData: setState,
+    syncTestDataInStorage
   };
 };
 
